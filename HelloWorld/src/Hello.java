743440f7bea25ccc1a2a5c36bde1fbdf4c0e4951
Md Rahaman <soikatesc@gmail.com>
2017-07-11 22:27:56 -0500

    Hello world

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mdrahaman on 7/11/17.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        int myFirstNumber = 5;
+        int mySecondNumber = 20;
+        int myThirdNumber = mySecondNumber * 2;
+        System.out.println(myThirdNumber);
+
+    }
+}
